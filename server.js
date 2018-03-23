3e8f1affcfa3a732571104aa9245d879275d5d8c
Pete Burns <peterburnshicks@gmail.com>
2018-03-22 23:58:33 -0400

    Building for production

--- server.js
+++ server.js
@@ -6,21 +6,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require("./routes");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(routes);
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {});
 
-// Send every request to the React app
-// Define any API routes before this runs
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("client/build"));
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
